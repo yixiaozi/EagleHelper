1bde4c491c42ffe6dca7b49b0c7a8197e34b9354
yixiaozi <wangyangwoaini@gmail.com>
2023-12-15 04:55:16 +0800

    。

--- EagleHelper.Designer.cs
+++ EagleHelper.Designer.cs
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(EagleHelper));
+            eagleFolderPath = new TextBox();
+            label1 = new Label();
+            openFileDialog_SelectPath = new OpenFileDialog();
+            btn_SelectFolder = new Button();
+            OnlyOneFolder = new Button();
+            SuspendLayout();
+            // 
+            // eagleFolderPath
+            // 
+            eagleFolderPath.Location = new Point(118, 12);
+            eagleFolderPath.Name = "eagleFolderPath";
+            eagleFolderPath.Size = new Size(448, 23);
+            eagleFolderPath.TabIndex = 0;
+            eagleFolderPath.Text = "H:\\Photos\\个人.library";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 15);
+            label1.Name = "label1";
+            label1.Size = new Size(100, 17);
+            label1.TabIndex = 1;
+            label1.Text = "Eagle库文件夹：";
+            // 
+            // openFileDialog_SelectPath
+            // 
+            openFileDialog_SelectPath.FileName = "openFileDialog1";
+            // 
+            // btn_SelectFolder
+            // 
+            btn_SelectFolder.Location = new Point(572, 12);
+            btn_SelectFolder.Name = "btn_SelectFolder";
+            btn_SelectFolder.Size = new Size(75, 23);
+            btn_SelectFolder.TabIndex = 2;
+            btn_SelectFolder.Text = "浏览";
+            btn_SelectFolder.UseVisualStyleBackColor = true;
+            btn_SelectFolder.Click += btn_SelectFolder_Click;
+            // 
+            // OnlyOneFolder
+            // 
+            OnlyOneFolder.Location = new Point(24, 236);
+            OnlyOneFolder.Name = "OnlyOneFolder";
+            OnlyOneFolder.Size = new Size(179, 44);
+            OnlyOneFolder.TabIndex = 3;
+            OnlyOneFolder.Text = "一个照片只保留一个文件夹";
+            OnlyOneFolder.UseVisualStyleBackColor = true;
+            OnlyOneFolder.Click += OnlyOneFolder_Click;
+            // 
+            // EagleHelper
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(659, 450);
+            Controls.Add(OnlyOneFolder);
+            Controls.Add(btn_SelectFolder);
+            Controls.Add(label1);
+            Controls.Add(eagleFolderPath);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "EagleHelper";
+            Text = "EagleHelper";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox eagleFolderPath;
+        private Label label1;
+        private OpenFileDialog openFileDialog_SelectPath;
+        private Button btn_SelectFolder;
+        private Button OnlyOneFolder;
     }
 }
--- EagleHelper.cs
+++ EagleHelper.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json.Linq;
+
 namespace EagleHelper
 {
     public partial class EagleHelper : Form
@@ -5,6 +7,34 @@ namespace EagleHelper
         public EagleHelper()
         {
             InitializeComponent();
+        }
+
+        private void OnlyOneFolder_Click(object sender, EventArgs e)
+        {
+            //遍历Eagle库文件夹所有metadata.json文件
+            string[] files = Directory.GetFiles(eagleFolderPath.Text + @"\images", "metadata.json", SearchOption.AllDirectories);
+            foreach (string file in files)
+            {
+                //读取metadata.json文件内容
+                string json = File.ReadAllText(file);
+                //反序列化为对象
+                JObject obj = JObject.Parse(json);
+                //如果folders个数多余一个，则保留第一个
+                if (obj["folders"].Count() > 1)
+                {
+                    obj["folders"] = new JArray(obj["folders"][0]);
+                    //序列化为json字符串
+                    string newJson = obj.ToString();
+                    //删除换行符
+                    newJson = newJson.Replace(Environment.NewLine, "");
+                    //写入文件
+                    File.WriteAllText(file, newJson);
+                }
+            }
+        }
+
+        private void btn_SelectFolder_Click(object sender, EventArgs e)
+        {
         }
     }
 }
